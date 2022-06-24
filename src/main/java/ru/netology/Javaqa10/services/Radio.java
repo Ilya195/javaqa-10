1400224250f5333aaadd24af7fcd1534ab67e3b9
Your Name <you@example.com>
2022-06-24 20:03:12 +0600

    Changed src

--- src/main/java/ru/netology/Javaqa10/services/Radio.java
+++ src/main/java/ru/netology/Javaqa10/services/Radio.java
@@ -3,44 +3,69 @@ package ru.netology.Javaqa10.services;
 public class Radio {
     protected int currentRadioStation;
     protected int currentVolume;
-    protected int maxStation;
+    protected int maxStation = 9;
 
-    public Radio(int stationscount){
-    }
     public Radio(){
         maxStation = 9;
+    }
+
+    public Radio(int stationscount){
+    maxStation = stationscount - 1;
     }
 
     public int getcurrentRadioStation() {
         return currentRadioStation;
     }
 
-    public void setCurrentRadioStationNext(int newCurrentRadioStation) {
-        if ( currentRadioStation < 9) {
-            currentRadioStation = currentRadioStation + 1;
-            }
+
+    public void setCurrentRadioStation(int newCurrentRadioStation) {
+        if (currentRadioStation < 0) {
+            return;
+        }
+        if (currentRadioStation > maxStation) {
+            return;
+        }
+        currentRadioStation = newCurrentRadioStation;
     }
 
-    public void setCurrentRadioStationDown(int newCurrentRadioStation) {
+
+    public void next() {
+        if ( currentRadioStation < maxStation) {
+            currentRadioStation  ++;
+        } else currentRadioStation = 0;
+    }
+
+    public void down() {
         if ( currentRadioStation > 0) {
-            currentRadioStation = currentRadioStation - 1;
+            currentRadioStation --;
+        } else currentRadioStation = 9; {
+
         }
     }
-
-
-
-
 
     public int getCurrentVolume() {
         return currentVolume;
     }
 
+    public void maxVolume() {
+        if (currentVolume < 100) {
+            currentVolume ++;
+        }
+    }
+
+    public void minVolume() {
+        if (currentVolume > 0) {
+            currentVolume --;
+        }
+    }
+
     public void setCurrentVolume(int newCurrentVolume) {
-        if (0 <= currentVolume && currentVolume < 10) {
-            currentVolume = newCurrentVolume + 1;
+        if (newCurrentVolume > 100 ) {
+            return;
         }
-        if (currentVolume >= 10) {
-            currentVolume = currentVolume - 1;
+        if (newCurrentVolume < 0) {
+            return;
         }
+        currentVolume = newCurrentVolume;
     }
 }
--- src/test/java/ru/netology/Javaqa10/services/RadioTest.java
+++ src/test/java/ru/netology/Javaqa10/services/RadioTest.java
@@ -5,12 +5,26 @@ import org.junit.jupiter.api.Test;
 
 public class RadioTest {
 
+
+
+    Radio service = new Radio();
+
     @Test
-    public void showCurrentRadioStation() {
+    public void showCurrentRadioStationNext9() {
         Radio service = new Radio();
+        service.setCurrentRadioStation(8);
+        service.next();
+        int expected = 9;
+        int actual = service.getcurrentRadioStation();
 
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void showCurrentRadioStationNext() {
+        Radio service = new Radio();
         service.setCurrentRadioStation(9);
-
+        service.next();
         int expected = 0;
         int actual = service.getcurrentRadioStation();
 
@@ -18,12 +32,88 @@ public class RadioTest {
     }
 
     @Test
-    public void showCurrentVolume() {
+    public void showCurrentRadioStationMax() {
         Radio service = new Radio();
+        service.setCurrentRadioStation(11);
+        service.next();
+        int expected = 0;
+        int actual = service.getcurrentRadioStation();
 
-        service.setCurrentVolume(10);
+        assertEquals(expected, actual);
+    }
 
-        int expected = 10;
+    @Test
+    public void showCurrentRadioStationPrev1() {
+        Radio service = new Radio();
+        service.setCurrentRadioStation(1);
+        service.down();
+        int expected = 0;
+        int actual = service.getcurrentRadioStation();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void showCurrentRadioStationPrev() {
+        Radio service = new Radio();
+        service.setCurrentRadioStation(0);
+        service.down();
+        int expected = 9;
+        int actual = service.getcurrentRadioStation();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void showCurrentRadioStationPrevZero() {
+        Radio service = new Radio();
+        service.setCurrentRadioStation(- 1);
+        service.down();
+        int expected = 9;
+        int actual = service.getcurrentRadioStation();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void showCurrentVolumeMax() {
+
+        service.setCurrentVolume(100);
+
+        int expected = 100;
+        int actual = service.getCurrentVolume();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void showCurrentVolumeMin() {
+
+        service.setCurrentVolume(0);
+
+        int expected = 0;
+        int actual = service.getCurrentVolume();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void showCurrentVolumePlus() {
+
+        service.setCurrentVolume(99);
+        service.maxVolume();
+        int expected = 100;
+        int actual = service.getCurrentVolume();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void showCurrentVolumeMinus() {
+
+        service.setCurrentVolume(1);
+        service.minVolume();
+        int expected = 0;
         int actual = service.getCurrentVolume();
 
         assertEquals(expected, actual);
